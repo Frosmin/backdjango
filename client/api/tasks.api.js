b7f4ebfa3e886269a886ee91721e858646abc003
unknown <simonabastomart@gmail.com>
2024-03-12 00:08:37 -0400

    coloacando comentarios

--- client/api/tasks.api.js
+++ client/api/tasks.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const getAllTasks = () => {
-  return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
+  return axios.get('http://localhost:8000/tasks/api/v1/tasks/')  // sirve para hacer peticiones a la API
 }
